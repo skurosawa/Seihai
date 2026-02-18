8b91426e131a77b08aa079a0520333281abaf54d
Shinsuke Kurosawa <veloiuria00@gmail.com>
2026-02-18 20:01:14 +0900

    Initial Commit

---
+++ Seihai/SeihaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeihaiApp.swift
+//  Seihai
+//
+//  Created by 黒澤信介 on 2026/02/18.
+//
+
+import SwiftUI
+
+@main
+struct SeihaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
